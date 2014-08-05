eb8d587ea27940ed75ae112a2caf3398eda1da21
Tushar Pokle <tushar.pokle@gmail.com>
2014-08-05 20:48:57 +1000

    build and install scripts - but no where to download it from yet

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojr "0.1.0"
+(defproject clojr "DEV"
   :description "Run single file clojure apps"
   :url "https://github.com/pokle/clojr"
   :license {:name "Eclipse Public License"
