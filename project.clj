abf891b365338116a3bb9fca94f9f3116fa882c0
tushar.pokle <tushar.pokle@gmail.com>
2015-11-29 05:08:47 -0500

    Upgraded clojure to 1.7.0

--- project.clj
+++ project.clj
@@ -1,14 +1,15 @@
-(defproject clojr "0.4.0"
+(defproject clojr "0.5.0"
   :description "Run single file clojure apps"
   :url "https://github.com/pokle/clojr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.cemerick/pomegranate "0.3.0"]
-                 [reply "0.3.4"]]
+                 [reply "0.3.7"]]
   :profiles {:uberjar {:aot :all}}
 
-  :plugins [[lein-pprint "1.1.1"]]
+  :plugins [[lein-pprint "1.1.1"]
+            [lein-ancient "0.6.8"]]
 
   :main reply.ReplyMain
 )
